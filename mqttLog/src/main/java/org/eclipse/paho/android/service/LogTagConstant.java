fc0459dd461aebad30bb4fa1c806945bcb19a6c9
Administrator <13025166246@163.com>
2018-05-23 10:17:14 +0800

    init commit

---
+++ mqttLog/src/main/java/org/eclipse/paho/android/service/LogTagConstant.java
@@ -0,0 +1,14 @@
+package org.eclipse.paho.android.service;
+
+/**
+ * ================================================
+ * 作    者：Luffy（张阳）
+ * 版    本：1.0
+ * 创建日期：2018/5/11
+ * 描    述：
+ * 修订历史：
+ * ================================================
+ */
+public class LogTagConstant {
+    public static final String TAG = "MqttlibMessage";
+}
